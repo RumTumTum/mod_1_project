d21bc07935af506d7ddfa0228d932ed075b30180
peter-haferl <peter.haferl@gmail.com>
2019-07-29 16:53:01 -0400

    delint get_imdb.py code

--- get_imdb.py
+++ get_imdb.py
@@ -1,28 +1,25 @@
+'''Retrieves cleaned imdb.title.basics dataframe'''
+
 import pandas as pd
 
-def get_imdb():
-    '''Retrieves cleaned imdb.title.basics dataframme'''
-    
-    imdb_title_basics = pd.read_csv("./data/imdb.title.basics.csv")
-    
-    imdb_title_basics = imdb_title_basics.loc[imdb_title_basics['genres'].isnull() == False]
-    
-    imdb_title_basics.genres = imdb_title_basics.genres.map(lambda x : str(x).split(","))
-    
-    unique_genres = []
 
+def get_imdb():
+    '''Retrieves cleaned imdb.title.basics dataframe'''
+    imdb_title_basics = pd.read_csv("./data/imdb.title.basics.csv")
+    imdb_title_basics = imdb_title_basics.loc[imdb_title_basics['genres']
+                                              .isnull() == False]
+    imdb_title_basics.genres = imdb_title_basics.genres.map(lambda x:
+                                                            str(x).split(","))
+    unique_genres = []
     for i in imdb_title_basics.genres:
         for j in i:
             if j in unique_genres:
                 continue
             else:
                 unique_genres.append(j)
-                
     genre_dict = {}
-
     for genre in unique_genres:
         genre_dict[genre] = []
-    
     for genre in unique_genres:
         bool_list = []
         for movie in imdb_title_basics.genres:
@@ -31,8 +28,6 @@ def get_imdb():
             else:
                 bool_list.append('False')
         genre_dict[genre] = bool_list
-    
     for genre in unique_genres:
         imdb_title_basics[genre] = genre_dict[genre]
-    
     return imdb_title_basics
