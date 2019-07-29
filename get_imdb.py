b0db8ebbf5c4240f4a365f6b09b528941c480c5c
peter-haferl <peter.haferl@gmail.com>
2019-07-29 16:31:37 -0400

    add genre boolean columns output of get_imdb

--- get_imdb.py
+++ get_imdb.py
@@ -1,11 +1,38 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 def get_imdb():
     '''Retrieves cleaned imdb.title.basics dataframme'''
+    
     imdb_title_basics = pd.read_csv("./data/imdb.title.basics.csv")
+    
     imdb_title_basics = imdb_title_basics.loc[imdb_title_basics['genres'].isnull() == False]
+    
     imdb_title_basics.genres = imdb_title_basics.genres.map(lambda x : str(x).split(","))
+    
+    unique_genres = []
+
+    for i in imdb_title_basics.genres:
+        for j in i:
+            if j in unique_genres:
+                continue
+            else:
+                unique_genres.append(j)
+                
+    genre_dict = {}
+
+    for genre in unique_genres:
+        genre_dict[genre] = []
+    
+    for genre in unique_genres:
+        bool_list = []
+        for movie in imdb_title_basics.genres:
+            if genre in movie:
+                bool_list.append('True')
+            else:
+                bool_list.append('False')
+        genre_dict[genre] = bool_list
+    
+    for genre in unique_genres:
+        imdb_title_basics[genre] = genre_dict[genre]
+    
     return imdb_title_basics
