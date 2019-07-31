57d4c04a290f16981da9b4b86adb6ec7725dd6f3
peter-haferl <peter.haferl@gmail.com>
2019-07-31 11:23:28 -0400

    bad commit message :(.. add database pickle, clean repo, add comments to clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -4,11 +4,14 @@ import pandas as pd
 
 def get_imdb():
     '''Retrieves cleaned imdb.title.basics dataframe'''
+    # Import relevant imdb database
     df_imdb = pd.read_csv("./data/imdb.title.basics.csv")
     # Remove rows where genre is NaN
     df_imdb.dropna(subset=['genres'], inplace=True)
+    # Split genres strings and create a list containing genre strings
     df_imdb.genres = df_imdb.genres.map(lambda x:
                                         str(x).split(","))
+    # Create a list containing unique genres
     unique_genres = []
     for i in df_imdb.genres:
         for j in i:
@@ -16,6 +19,8 @@ def get_imdb():
                 continue
             else:
                 unique_genres.append(j)
+    # Create a dict containing unique genres as keys and lists
+    # of one-hot encoding for movie genres as values
     genre_dict = {}
     for genre in unique_genres:
         genre_dict[genre] = []
@@ -27,8 +32,10 @@ def get_imdb():
             else:
                 one_hot.append(0)
         genre_dict[genre] = one_hot
+    # Add columns to dataframe of one-hot encoding of genres
     for genre in unique_genres:
         df_imdb[genre] = genre_dict[genre]
+    # Make all column names snake_case
     new_columns = df_imdb.columns.map(lambda x: x.replace('-', '_').lower())
     df_imdb.columns = new_columns
     return df_imdb
@@ -36,39 +43,41 @@ def get_imdb():
 
 def tn_movie_budgets(cutoff=1):
     '''Imports and cleans tn database'''
+    # Import The Numbers movie budget database
     df_tn = pd.read_csv('data/tn.movie_budgets.csv')
-    # Convert object in format $10,000 to float64
+    # Convert object in format $10,000 to float64 for domestic_gross column
     df_tn.domestic_gross = df_tn.domestic_gross.replace(
         '[$,]',
         '',
         regex=True
     ).astype(float)
-    # Convert object in format $10,000 to float64
+    # Convert object in format $10,000 to float64 for worldwide_gross column
     df_tn.worldwide_gross = df_tn.worldwide_gross.replace(
         '[$,]',
         '',
         regex=True
     ).astype(float)
-    # Convert object in format $10,000 to float64
+    # Convert object in format $10,000 to float64 for production_budget column
     df_tn.production_budget = df_tn.production_budget.replace(
         '[$,]',
         '',
         regex=True
     ).astype(float)
-    # Convert date to date
+    # Create new df with only movies above the worldwide_gross cut-off
     df_tn_cutoff = df_tn.loc[df_tn['worldwide_gross'] > cutoff].copy()
-    # Create new df with only movies above the revenue cut-off
+    # Convert release_date to datetime data type
     df_tn_cutoff['release_date'] = pd.to_datetime(df_tn_cutoff.release_date)
+    # Make new column with start year of movie
     df_tn_cutoff['start_year'] = df_tn_cutoff.release_date.dt.year
-    # Return a cleaned and sorted tn_movie_budgets
     return df_tn_cutoff
 
 
 def movies_combined():
     '''Merge imdb and tn database'''
+    # Import The numbers database and relevant imdb database
     df_tn = tn_movie_budgets()
     df_imdb = get_imdb()
-    # Create new df joining df_TN and df_IMDB on movie title and year
+    # Create new df joining df_tn and df_imdb on title and year
     df_comb = pd.merge(df_imdb, df_tn, how='inner',
                        left_on=['original_title', 'start_year'],
                        right_on=['movie', 'start_year'])
@@ -76,9 +85,9 @@ def movies_combined():
     # (removes many NaN and <30 minute cases)
     df_comb.sort_values(by='runtime_minutes', ascending=False)
     df_comb.drop_duplicates(subset='primary_title', keep='first', inplace=True)
-    # Only take movies that have been released and had time to make money
+    # Subset movies that have 2010 - 2018 start years
     df_comb = df_comb.loc[df_comb['start_year'] < 2019].copy()
-    # Remove irrelevant rows
+    # Remove irrelevant genre columns
     df_comb.drop(columns=['news', 'adult', 'talk_show', 'reality_tv',
                           'game_show', 'short'], inplace=True)
     return df_comb
@@ -86,6 +95,7 @@ def movies_combined():
 
 def movies_combined_cutoff(cutoff):
     '''Merge imdb and tn database'''
+    # Import The numbers database and relevant imdb database
     df_tn = tn_movie_budgets(cutoff)
     df_imdb = get_imdb()
     # Create new df joining df_TN and df_IMDB on movie title and year
@@ -96,9 +106,9 @@ def movies_combined_cutoff(cutoff):
     # (remove many NaN and <30 minute cases)
     df_comb.sort_values(by='runtime_minutes', ascending=False)
     df_comb.drop_duplicates(subset='primary_title', keep='first', inplace=True)
-    # Only take movies that have been released and had time to make money
+    # Subset movies that have 2010 - 2018 start years
     df_comb = df_comb.loc[df_comb['start_year'] < 2019]
-    # Remove irrelevant rows
+    # Remove irrelevant genre columns
     df_comb.drop(columns=['news', 'adult', 'talk_show', 'reality_tv',
                           'game_show', 'short'], inplace=True)
     return df_comb
